e9c1eedcf217117d11d655b1cdc13f4460860e4a
Christopher Hunt <huntchr@gmail.com>
2014-02-07 15:30:03 +1100

    Bumping version to snapshot.

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object BuildSettings {
     organization := "org.webjars",
     name := "webjars-play",
     scalaVersion := "2.10.3",
-    version := "2.2.1",
+    version := "2.2.2-SNAPSHOT",
     resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
     resolvers += "Local Maven" at Path.userHome.asFile.toURI.toURL + ".m2/repository",
     libraryDependencies ++= Seq(
