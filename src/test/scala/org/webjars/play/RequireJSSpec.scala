d1ce2e4aa5c47fa58ecc140be1375d49367e525b
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-11-27 20:23:07 +0100

    Bump play to 2.7.0-RC3 (and few other version bumps)

--- src/test/scala/org/webjars/play/RequireJSSpec.scala
+++ src/test/scala/org/webjars/play/RequireJSSpec.scala
@@ -14,7 +14,7 @@ class RequireJSSpec extends PlaySpecification {
       status(result) must equalTo(OK)
       contentType(result) must beSome(MimeTypes.JAVASCRIPT)
       contentAsString(result) must contain("return ['/webjars/' + webJarId")
-      contentAsString(result) must contain("\"requirejs\":\"2.3.5\"")
+      contentAsString(result) must contain("\"requirejs\":\"2.3.6\"")
     }
     "produce a setup using a cdn" in new WithApplication(_.configure("webjars.use-cdn" -> "true")) {
       val requireJs = app.injector.instanceOf[RequireJS]
